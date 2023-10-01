e94c62ca68d1693486b8ea2cec3246a937fef459
Olga Mamonova <shamrilo91@mail.ru>
2023-10-02 01:22:22 +0300

    добавляет адаптеры рефактор модели

--- src/mocks/const.js
+++ src/mocks/const.js
@@ -123,6 +123,40 @@ const Method = {
   DELETE: 'DELETE',
 };
 
+const adaptToClient = (point) => {
+  const adaptedPoint = {
+    ...point,
+    dateFrom: point['date_from'],
+    dateTo: point['date_to'],
+    basePrice: point['base_price'],
+    isFavorite: point['is_favorite'],
+  };
+
+  delete adaptedPoint['date_from'];
+  delete adaptedPoint['date_to'];
+  delete adaptedPoint['base_price'];
+  delete adaptedPoint['is_favorite'];
+
+  return adaptedPoint;
+};
+
+const adaptToServer = (point) => {
+  const adaptedPoint = {
+    ...point,
+    ['date_from']: new Date(point.dateFrom).toISOString(),
+    ['date_to']: new Date(point.dateTo).toISOString(),
+    ['base_price']: point.basePrice,
+    ['is_favorite']: point.isFavorite,
+  };
+
+  delete adaptedPoint.dateFrom;
+  delete adaptedPoint.dateTo;
+  delete adaptedPoint.basePrice;
+  delete adaptedPoint.isFavorite;
+
+  return adaptedPoint;
+};
+
 export {
   POINT_COUNT,
   CITIES,
@@ -145,5 +179,7 @@ export {
   EditType,
   POINT_EMPTY,
   MODE,
-  Method
+  Method,
+  adaptToClient,
+  adaptToServer
 };
